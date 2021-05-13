7ce7286035537b1ef3dab6749390c09e5d0f2151
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2021-05-13 11:05:09 -0300

    Update-2

--- main.lua
+++ main.lua
@@ -9,6 +9,10 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
@@ -25,7 +29,15 @@ end
 function love.draw()
     push:apply('start')
 
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply('end')
 end
