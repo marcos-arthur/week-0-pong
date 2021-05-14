f3bfa2b898d89b803d8c460c6c11daefc08a1419
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2021-05-14 11:47:26 -0300

    Update-Final

--- Ball.lua
+++ Ball.lua
@@ -6,8 +6,8 @@ function Ball:init(x, y, width, height)
     self.width = width
     self.height = height
 
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(2) == 1 and math.random(-80, -200) or math.random(80, 200)
+    self.dy = 0
+    self.dx = 0
 end
 
 function Ball:collides(paddle)
@@ -25,8 +25,8 @@ end
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dy = 0
+    self.dx = 0
 end
 
 function Ball:update(dt)
--- main.lua
+++ main.lua
@@ -37,18 +37,21 @@ function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = true,
-        vsync = true
+        vsync = true,
+        canvas = false
     })
+
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     player1Score = 0
     player2Score = 0
 
     servingPlayer = 1
 
-    player1 = Paddle(10, 30, 5, 20)
-    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
-
-    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    winningPlayer = 0
 
     gameState = 'start'
 end
@@ -185,13 +188,9 @@ function love.keypressed(key)
 end
 
 function love.draw()
-    push:apply('start')
+    push:start()
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
-
-    love.graphics.setFont(smallFont)
-
-    displayScore()
 
     if gameState == 'start' then
         love.graphics.setFont(smallFont)
@@ -210,28 +209,25 @@ function love.draw()
         love.graphics.printf('Press Enter to restart!', 0, 30, VIRTUAL_WIDTH, 'center')
     end
 
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    displayScore()
 
     player1:render()
     player2:render()
-
     ball:render()
 
     displayFPS()
 
-    push:apply('end')
-end
-
-function displayFPS()
-    love.graphics.setFont(smallFont)
-    love.graphics.setColor(0, 1, 0, 1)    
-    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+    push:finish()
 end
 
 function displayScore()
     love.graphics.setFont(scoreFont)
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 1, 0, 1)    
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
