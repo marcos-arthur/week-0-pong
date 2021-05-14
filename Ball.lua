888b27ba44bd4c4be852125e35539675c184e660
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2021-05-14 10:52:16 -0300

    Update-7

--- Ball.lua
+++ Ball.lua
@@ -7,7 +7,7 @@ function Ball:init(x, y, width, height)
     self.height = height
 
     self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and math.random(-80, -200) or math.random(80, 200)
 end
 
 function Ball:collides(paddle)
